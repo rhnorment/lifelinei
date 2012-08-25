499633cb562d4039d0b5117ba8bb9c4eaa4ec05c
rhnorment <norment@gmail.com>
2012-08-25 15:26:00 -0500

    Modified configuration.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery
+
+  private
+
+  before_filter :controller_and_action_names
+
+      def controller_and_action_names
+        @current_action = action_name
+        @current_controller = controller_name
+      end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+
+  # return a title on a per-page basis:
+  # Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = "LifeLine Incorporated"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+
+  def get_controller_klass(controller)
+    if controller == @current_controller
+      "current_page_item"
+    else
+      ""
+    end
+  end
+
+  # default form builder:
+  def labeled_form_for(object, options = {}, &block)
+    options[:builder] = LabeledFormBuilder
+    form_for(object, options, &block)
+  end
+
 end
