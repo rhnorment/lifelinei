e31b7cd2c68cdee35a254ed254ce44efde124550
rhnorment <norment@gmail.com>
2012-09-11 15:46:03 -0500

    added class survey model and table.

---
+++ app/models/survey.rb
@@ -0,0 +1,27 @@
+# == Schema Information
+#
+# Table name: surveys
+#
+#  id                   :integer         not null, primary key
+#  survey_code          :string(255)
+#  business_name        :string(255)
+#  knowledge_rating     :integer
+#  knowledge_comment    :text
+#  availability_rating  :integer
+#  availability_comment :text
+#  instructor_rating    :integer
+#  instructor_comment   :text
+#  conditions_rating    :integer
+#  condition_comment    :text
+#  opinion_rating       :integer
+#  opinion_comment      :text
+#  created_at           :datetime        not null
+#  updated_at           :datetime        not null
+#
+
+class Survey < ActiveRecord::Base
+
+  # class attriibutes:
+  attr_accessible        :survey_code, :business_name, :knowledge_comment, :knowledge_rating
+
+end
