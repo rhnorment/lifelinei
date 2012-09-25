1515e21c4d308b46707cd51f06133c959dbca733
rhnorment <norment@gmail.com>
2012-09-25 16:01:52 -0500

    fixed survey form formatting.

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -15,6 +15,13 @@ class SurveysController < ApplicationController
 
   def create
     @survey = Survey.new(params[:survey])
+    if @survey.save
+      flash.now[:green] = "Your feedback was successfully sent to LifeLine!."
+      render :new
+    else
+      flash.now[:red] = "There was a problem submitting your feedback.  Please try again."
+      render :new
+    end
   end
 
   def destroy
