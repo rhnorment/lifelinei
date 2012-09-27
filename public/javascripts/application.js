87aa68deaf624945d6e6e428c55a0da24b6f7c4f
Colin Smith <colin@blkcat.net>
2012-09-27 19:17:49 -0400

    Form JS

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,28 @@
-$(document).ready(function() {
-    setTimeout(hideFlashMessages, 15000);
-});
+( function( $ ) {
+        $(document).ready(function() {
+            // automatically hide flash messages
+            setTimeout( function() {
+                $( 'p.message_red' ).fadeOut( 3000 );
+            }, 15000 );
 
-function hideFlashMessages() {
-    $('p.message_red').fadeOut(3000);
-}
+            $( 'input.toggleComment' )
+                .closest( '.element-wrapper' )
+                .next( '.element-wrapper' )
+                .hide();
+   
+            $( 'input.toggleComment' ).change( function() {
+                var elem = $( this );
+                var parent = elem.closest( '.element-wrapper' );
 
+                var nextContainer = parent.next( '.element-wrapper' );
+
+                var value = elem.val();
+
+                if( ( ! value ) || ( value > 9 ) ) {
+                    nextContainer.show();
+                } else {
+                    nextContainer.hide();
+                }
+            });
+        });
+})( jQuery )
