c418b89a8edbc9ec443eb7ea404c02d6b9fc0496
rhnorment <norment@gmail.com>
2012-09-28 16:06:54 -0500

    changed contact controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -20,8 +20,8 @@ class ContactsController < ApplicationController
     if @contact.save
       ContactMailer.contact_confirmation(@contact).deliver
       ContactMailer.contact_message(@contact).deliver
-      flash[:green] = "Your message was successfully sent to LifeLine!  Someone will be in touch with you shortly."
-      redirect_to :new
+      flash.now[:green] = "Your message was successfully sent to LifeLine!  Someone will be in touch with you shortly."
+      render :new
     else
       flash.now[:red] = "There was a problem submitting your message.  Every field is required.  Please try again."
       render :new
