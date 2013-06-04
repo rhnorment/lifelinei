24f073386f2bd24125cec95f8617e7e0c6f6092e
rhnorment <norment@gmail.com>
2013-06-04 13:38:45 -0500

    added customer login views and controller.

--- app/controllers/portals_controller.rb
+++ app/controllers/portals_controller.rb
@@ -1,31 +1,31 @@
 class PortalsController < ApplicationController
 
   def lifeline
-    @title = 'Lifeline Customer Portal'
+    @title = 'LifeLine Customer Portal'
   end
 
   def jli
-    @title = 'Lifeline Customer Portal'
+    @title = 'LifeLine Customer Portal'
   end
 
   def mayo
-    @title = 'Lifeline Customer Portal'
+    @title = 'LifeLine Customer Portal'
   end
 
   def ems
-    @title = 'Lifeline Customer Portal'
+    @title = 'LifeLine Customer Portal'
   end
 
   def landolakes
-    @title = 'Lifeline Customer Portal'
+    @title = 'LifeLine Customer Portal'
   end
 
   def ampride
-    @title = 'Lifeline Customer Portal'
+    @title = 'LifeLine Customer Portal'
   end
 
   def w9
-    @title = 'Lifeline Customer Portal'
+    @title = 'LifeLine Customer Portal'
   end
 
 end
