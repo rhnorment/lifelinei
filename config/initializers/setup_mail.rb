fa644bf531ea79b2c7939704520ac061a4a4a1a1
rhnorment <norment@gmail.com>
2013-06-28 12:45:46 -0500

    changed smtp settings.

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,11 +1,11 @@
 ActionMailer::Base.smtp_settings = {
-    :address => "smtp.secureserver.net",
-    :port => 25,
-    :domain => "lifelinei.com",
-    :user_name => "support@lifelinei.com",
-    :password => "soccer",
-    :authentication => "plain",
+    :address => 'smtp.secureserver.net',
+    :port => 80,
+    :domain => 'lifelinei.com',
+    :user_name => 'support@lifelinei.com',
+    :password => 'soccer',
+    :authentication => 'plain',
     :enable_starttls_auto => true
 }
-ActionMailer::Base.default_url_options[:host] = "lifelinei.com"
+ActionMailer::Base.default_url_options[:host] = 'lifelinei.com'
 Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
